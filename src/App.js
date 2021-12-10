0c3f35eaa58a132ecee727d1d3af188d3dc07ea8
mohaiminul69 <mohaiminul69@gmail.com>
2021-12-10 18:11:22 +0600

    action and reducers added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { Button } from "react-bootstrap";
+import { increment, decrement } from "./Actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <Button onClick={() => dispatch(increment())} variant="dark">
+        + Increment
+      </Button>
+      <Button
+        onClick={() => dispatch(decrement())}
+        className="ms-3"
+        variant="dark"
+      >
+        - Decrement
+      </Button>
     </div>
   );
 }
